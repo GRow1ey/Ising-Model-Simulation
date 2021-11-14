0999bb46515c7ddb528de8dd4ffe85e8a260f121
s1705159 <s1705159@sms.ed.ac.uk>
2021-11-14 07:38:15 +0000

    Add modifications to observable calculations to calculate the mean energy and magnetisations.

--- ising_model_calculate_observables.py
+++ ising_model_calculate_observables.py
@@ -12,13 +12,13 @@ def main():
         
   # Parse the command line arguments required to animate the Ising model.
   lattice_dimensions = int(sys.argv[1])
-  temperature = float(sys.argv[2])
-  dynamical_rule = sys.argv[3]
+  dynamical_rule = sys.argv[2]
   
   # Instantiate an object of the SpinLattice class.
   ising_model = SpinLattice(lattice_dimensions)
   
   if dynamical_rule == "Glauber":
     ising_model.calculate_observables(dynamical_rule)
+    ising_model.plot_mean_energy_against_temperature(dynamical_rule)
     
 main()
